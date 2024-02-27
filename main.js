fd82d0de2ab6c9b8114eb388dcb24b63e393e17d
rijuly <136062565+rijuly@users.noreply.github.com>
2024-02-27 18:02:52 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-song="";
+song1="";
+song2="";
 
 function preload()
 {
@@ -12,6 +13,9 @@ function setup() {
 
     video=createCapture(VIDEO);
     video.hide();
+    
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
@@ -21,4 +25,24 @@ function draw() {
 function play()
 {
     song.play();
+}
+
+function gotPoses (results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " +leftWristX +"leftWristY = "+leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " +rightWristX +"rightWristY = "+rightWristY);
+    }
+}
+
+function modelLoaded ()
+{
+    console.log('PoseNet Is Initialised')
 }
